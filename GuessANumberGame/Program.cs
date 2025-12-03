7c6642412b421e0ea3814793ed23c67e06d1040b
milboom123 <45424145+milboom123@users.noreply.github.com>
2025-12-03 10:06:21 +0100

    Add files via upload

---
+++ GuessANumberGame/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace GuessANumberGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int guess = 0;
+
+            int minNumber, maxNumber;
+
+            // Get minimum number
+            Console.WriteLine("Enter the minimum number:");
+            while (!int.TryParse(Console.ReadLine(), out minNumber))
+            {
+                Console.WriteLine("Please enter a valid number:");
+            }
+
+            // Get maximum number
+            Console.WriteLine("Enter the maximum number:");
+            while (!int.TryParse(Console.ReadLine(), out maxNumber) || maxNumber <= minNumber)
+            {
+                Console.WriteLine("Please enter a valid number greater than the minimum:");
+            }
+
+            // Generate the random number within user range
+            int NumberRange = rand.Next(minNumber, maxNumber + 1);
+
+            Console.WriteLine($"Guess a number between {minNumber} and {maxNumber}");
+
+            int attempts = 0;
+
+            // Guessing loop
+            while (guess != NumberRange)
+            {
+                try
+                {
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    attempts++;
+
+                    if (guess > NumberRange)
+                    {
+                        Console.WriteLine("Too High");
+                    }
+                    else if (guess < NumberRange)
+                    {
+                        Console.WriteLine("Too Low");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You guessed it! It took you {attempts} tries");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Guess must be a number");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
